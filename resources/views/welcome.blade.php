3a6dfd49417214483f758a222ba473c6fc834ba5
kuma-coffee <daniel099d@gmail.com>
2023-12-09 03:37:49 +0700

    add css and image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
     @endif
 
     <div class="content">
+        <img src="{{asset('img/pizza-house.png')}}" alt="pizza-house">
         <div class="title m-b-md">
-            Pizza House<br />
             The North's Best Pizzas
         </div>
     </div>
