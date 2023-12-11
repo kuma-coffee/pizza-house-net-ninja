212d3049afefdb7ce19691414497a43898c2d4c9
kuma-coffee <daniel099d@gmail.com>
2023-12-12 01:32:41 +0700

    add controller, connect to mysql db, add migration, use eloquent models, MVC name convention

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pizza;
+
+class PizzaController extends Controller
+{
+    public function index()
+    {
+        // $pizzas = Pizza::all();
+        // $pizzas = Pizza::orderBy('name', 'desc')->get();
+        // $pizzas = Pizza::where('type', 'hawaiian')->get();
+        $pizzas = Pizza::latest()->get();
+
+        return view('pizzas.index', ['pizzas' => $pizzas]);
+    }
+
+    public function show($id)
+    {
+        return view('pizzas.show', ['id' => $id]);
+    }
+
+    public function create()
+    {
+        return view('pizzas.create');
+    }
+}
